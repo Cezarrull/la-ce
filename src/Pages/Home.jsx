f3a542080ec9ae1b834db47a2294974eaf9cd4d4
Cezar rull <cezarrull12345@gmail.com>
2025-03-15 12:23:45 -0300

    :wrench: add new content box music and adjustments

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import {Header} from '../Components/Header'
 
+/*import fixYou from '../Audios/fixYou.mp3'*/
+import fixyouImg from '../Assets/fixyou.jpg'
+
+import play from '../Assets/play.svg'
+import pause from '../Assets/pause.svg'
+import next from '../Assets/caret-line-right-fill.svg'
+import back from '../Assets/caret-line-left-fill.svg'
+
 export function Home() {
     const [activeNight, setActiveNight] = useState(true);
+    const [playing, setPlaying] = useState(true);
 
     const toggleNightMode = (activeNight) => {
         setActiveNight(activeNight);
+    }
+
+    const handleButtonPlay = () => {
+        setPlaying(!playing);
     }
 
     return(
@@ -13,7 +26,31 @@ export function Home() {
             <Header toggleNightMode={toggleNightMode} />
 
             <div className={`homeBody ${activeNight ? "homeNight" : "homeDay"}`}>
+                <div className="bodyMusic">
+                    <div className="bgMusic">
+                        <div className="boxMusic">
+                            <img src={fixyouImg} alt="capa fix you" className="imageMusic"/>
+                            <div className='bodyMusicControls'>
+                                <div className='infoMusic'>
+                                    <h2>Fix you</h2>
+                                </div>
+                                
+                                <div className="musicControls">
+                                    <button className="buttonMusic"><img src={back} alt="voltar" className="back"/></button>
+                                    <button className="buttonMusic" onClick={handleButtonPlay}>
+                                        <img src={play} alt="play" className={playing ? "play" : "playOn"}/>
+                                        <img src={pause} alt="pause" className={playing ? "pause" : "pauseOn"}/>
+                                    </button>
+                                    <button className="buttonMusic"><img src={next} alt="próxima" className="next"/></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
 }
+                /*<audio preload='metadata' controls >
+                    <source src={fixYou} type="audio/mpeg"/>
+                </audio>*/
